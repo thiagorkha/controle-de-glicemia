ec37b5a2b4146847583cac941ab5ce9c3280d134
thiagorkha <91204179+thiagorkha@users.noreply.github.com>
2025-08-01 07:16:04 -0300

    Update GlicemiaForm.js

--- frontend/src/components/GlicemiaForm.js
+++ frontend/src/components/GlicemiaForm.js
@@ -16,7 +16,7 @@ function GlicemiaForm() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:5000/medicao', {
+      const response = await axios.post('https://controle-de-glicemia.onrender.com', {
         data,
         tipo_medicao: tipoMedicao,
         valor: parseFloat(valor),
