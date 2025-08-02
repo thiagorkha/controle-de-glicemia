43b94127cafbd1b1107c5eae6eda524aa84e90f7
thiagorkha <91204179+thiagorkha@users.noreply.github.com>
2025-08-02 09:17:13 -0300

    Create constants.js

---
+++ frontend/src/utils/constants.js
@@ -0,0 +1,10 @@
+export const GLICEMIA_TIPOS = [
+  'Glicemia Jejum',
+  'Glicemia 2h após café',
+  'Glicemia antes do almoço',
+  'Glicemia 2h após almoço',
+  'Glicemia antes do jantar',
+  'Glicemia 2h após o jantar',
+  'Glicemia ao deitar',
+  'Glicemia as 3:00',
+];
